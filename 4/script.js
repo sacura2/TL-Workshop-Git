c20c94210cf7bfef7156b0bad1e3641a02b3bfc9
Alexandra Tetzlaff <al.tetzlaff@outlook.de>
2024-11-25 18:52:52 +0100

    project4

---
+++ 4/script.js
@@ -0,0 +1,34 @@
+let timer = document.getElementById("timer");
+let progressBar = document.getElementById("progress-bar");
+let interval;
+let seconds = 0;
+
+function startTimer() {
+    if (!interval) {
+        interval = setInterval(updateTimer, 1000);
+    }
+}
+
+function stopTimer() {
+    clearInterval(interval);
+    interval = null;
+}
+
+function resetTimer() {
+    stopTimer();
+    seconds = 0;
+    timer.innerHTML = "00:00:00";}
+
+function updateTimer() {
+    seconds++;
+    let hours = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds % 3600) / 60);
+    let secs = seconds % 60;
+
+    timer.innerHTML = `${pad(hours)}:${pad(mins)}:${pad(secs)}`;
+    
+}
+
+function pad(num) {
+    return num.toString().padStart(2, '0');
+}
